80ff043f25ab9164796d9bbde383a2845b555922
ldanielam <lamarinb@unal.edu.co>
2024-12-09 15:31:16 -0500

    Punto Tres con comentarios

--- puntotres.py
+++ puntotres.py
@@ -1,17 +1,16 @@
-k1 = 25000000
-k2 = 18900000
-tasa1 = 0.20
-tasa2 = 0.30
-def simple1():
-    intsimp = k1 * tasa1 * 360
-    interes_simple = k1 + intsimp
-    return interes_simple
-def simple2():
-    intsimp = k2 * tasa2 * 360
-    interes_simple = k2 + intsimp
-    return interes_simple
-
-contador=2022
-while simple1()>simple2():
-    contador +=1
-print("El año en el que la población A supera a la población B es:",contador)
+k1 = 25000000 #Valor de A
+k2 = 18900000 #Valor de B
+tasa1 = 0.20 #Tasa de A
+tasa2 = 0.30 #Tasa de B
+def simple1(): #Función que permite evidenciar el crecimiento de la población A
+    intsimp = k1 * tasa1 * 360 #La variable es igual a A * tasa de A * 360 días
+    interes_simple = k1 + intsimp #La variable suma A y el crecimiento anual
+    return interes_simple #Se devuelve el valor
+def simple2(): #La función permite evidenciar el crecimiento de la población B
+    intsimp = k2 * tasa2 * 360 #La variable es igual a B * tasa de B * 360 días
+    interes_simple = k2 + intsimp #La variable suma B y el crecimiento anual
+    return interes_simple #Se devuelve el valor
+contador = 2022 #Se establece un contador en el año de inicio
+if simple2()>simple1():#Condición de que si B pasa a A
+    contador += 1#Se suma un año
+    print("El año en que lo supera es:",contador)#Mostrar el mensaje
